22607497e6276b9923b818461e77ccd25aa587d9
Zak Kastl <zkastl8@gmail.com>
2026-06-18 14:41:53 -0500

    started adding details for imdb text classification

---
+++ imdb_dataset.py
@@ -0,0 +1,12 @@
+"""
+imdb_dataset.py
+
+Class and Function definitions for 
+"""
+# pylint: disable=C0116, C0103, C0155
+
+class IMDBFromScratch:
+    pass
+
+def load_imdb(directory):
+    return None, None
--- main.py
+++ main.py
@@ -7,34 +7,39 @@ import uuid
 from torch.utils.data import DataLoader
 
 from mnist_from_scratch import MNISTFromScratch
+from imdb_dataset import load_imdb
 from model import ConvolutionalModel, LinearModel
 
 ###########################
 # Dataset Preparation
-TRAIN_IMAGE_FILEPATH = '/home/zak/data/mnist/train-images.idx3-ubyte'
-TRAIN_LABEL_FILEPATH = '/home/zak/data/mnist/train-labels.idx1-ubyte'
-TEST_IMAGE_FILEPATH = '/home/zak/data/mnist/t10k-images.idx3-ubyte'
-TEST_LABEL_FILEPATH = '/home/zak/data/mnist/t10k-labels.idx1-ubyte'
+TRAIN_IMAGE_FILEPATH    = '/home/zak/data/mnist/train-images.idx3-ubyte'
+TRAIN_LABEL_FILEPATH    = '/home/zak/data/mnist/train-labels.idx1-ubyte'
+TEST_IMAGE_FILEPATH     = '/home/zak/data/mnist/t10k-images.idx3-ubyte'
+TEST_LABEL_FILEPATH     = '/home/zak/data/mnist/t10k-labels.idx1-ubyte'
+
+TRAIN_TEXT_DIR          = '/home/zak/data/imdb/train'
+TEST_TEXT_DIR           = '/home/zak/data/imdb/test'
+
 BATCH_SIZE = 64
 NUM_EPOCHS = 40
+UNIQUE_ID = uuid.uuid7().int
 
+# IMAGE DATASET
 train_dataset = MNISTFromScratch(image_path=TRAIN_IMAGE_FILEPATH, label_path=TRAIN_LABEL_FILEPATH)
 test_dataset = MNISTFromScratch(image_path=TEST_IMAGE_FILEPATH, label_path=TEST_LABEL_FILEPATH)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
 image_train, label_train = next(iter(train_loader))
 image_test, label_test = next(iter(test_loader))
 
-assert len(train_dataset) == 60000
-assert len(test_dataset) == 10000
-###########################
+# TEXT DATASET
+train_texts, train_labels = load_imdb(TRAIN_TEXT_DIR)
 
-###########################
-# Model Preparation
 
-# generate a unique id for tracking individual runs
-UNIQUE_ID = uuid.uuid7().int
+
+# MODELS
+
+
 
 # Train and save model 1
 model = LinearModel(lr=0.01)
