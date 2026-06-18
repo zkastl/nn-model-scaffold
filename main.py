fa4fb0b691d7a00c634b990b30e31a2d2f624b03
Zak Kastl <zkastl8@gmail.com>
2026-06-18 13:09:41 -0500

    added code to the model to track training history

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ main.py
 
 Entry Point for Application
 """
-#import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
 from mnist_from_scratch import MNISTFromScratch
@@ -15,11 +14,13 @@ TRAIN_IMAGE_FILEPATH = '/home/zak/data/mnist/train-images.idx3-ubyte'
 TRAIN_LABEL_FILEPATH = '/home/zak/data/mnist/train-labels.idx1-ubyte'
 TEST_IMAGE_FILEPATH = '/home/zak/data/mnist/t10k-images.idx3-ubyte'
 TEST_LABEL_FILEPATH = '/home/zak/data/mnist/t10k-labels.idx1-ubyte'
+BATCH_SIZE = 64
+NUM_EPOCHS = 40
 
 train_dataset = MNISTFromScratch(image_path=TRAIN_IMAGE_FILEPATH, label_path=TRAIN_LABEL_FILEPATH)
 test_dataset = MNISTFromScratch(image_path=TEST_IMAGE_FILEPATH, label_path=TEST_LABEL_FILEPATH)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 image_train, label_train = next(iter(train_loader))
 image_test, label_test = next(iter(test_loader))
@@ -32,4 +33,7 @@ assert len(test_dataset) == 10000
 # Model Preparation
 
 model = LinearModel(lr=0.01)
-model.train_model(train_loader, test_loader, num_epochs=50)
+model.train_model(train_loader, test_loader, num_epochs=NUM_EPOCHS)
+
+# print out training chart
+model.display_training_history(output_file='./fig.png')
--- model.py
+++ model.py
@@ -5,7 +5,10 @@ for all your modelling needs
 """
 # pylint: disable=C0116, C0103
 
+from time import time as py_time
+
 import torch
+import matplotlib.pyplot as plt
 from torch import nn, optim, no_grad
 
 class LinearModel(nn.Module):
@@ -20,12 +23,15 @@ class LinearModel(nn.Module):
             nn.ReLU(),
             nn.Linear(512,512),
             nn.ReLU(),
+            nn.Linear(512,512),
+            nn.ReLU(),
             nn.Linear(512, 10)
         )
 
         self.learning_rate = lr
         self.optimizer = optim.SGD(self.parameters(), lr=self.learning_rate)
         self.loss_fn = nn.CrossEntropyLoss()
+        self.history = {'loss': [], 'training_time': 0.0}
 
     def forward(self, x):
         """
@@ -35,11 +41,23 @@ class LinearModel(nn.Module):
         logits = self.linear_reul_stack(x)
         return logits
 
-    def train_model(self, train_loader, test_loader, num_epochs=10):
+    def train_model(self, train_loader, test_loader, num_epochs):
+
+        # clear training history
+        self.history['loss'] = []
+        self.history['training_time'] = 0.0
+
+        # For each epoch, run the training loop, then run the test (really validation) loop
+        start_time = py_time()
         for t in range(num_epochs):
             print(f'EPOCH {t+1}\n-----------------------------')
             self.train_loop(train_loader)
-            self.test_loop(test_loader)
+            test_loss = self.test_loop(test_loader)
+
+            self.history['loss'].append(test_loss)
+
+        end_time = py_time()
+        self.history['training_time'] = end_time - start_time
         print('DONE!')
 
 
@@ -62,7 +80,7 @@ class LinearModel(nn.Module):
                 loss, current = loss.item(), batch * dataloader.batch_size + len(X)
                 print(f'loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
 
-    def test_loop(self, dataloader):
+    def test_loop(self, dataloader) -> float:
         self.eval()
         size = len(dataloader.dataset)
         num_batches = len(dataloader)
@@ -77,3 +95,10 @@ class LinearModel(nn.Module):
         test_loss /= num_batches
         correct /= size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+        return test_loss
+
+    def display_training_history(self, output_file='./fig.png'):
+        plt.plot(range(len(self.history['loss'])), self.history['loss'])
+        plt.title(f'Training Time: {self.history['training_time']:>2f} Seconds')
+        plt.savefig(output_file)
